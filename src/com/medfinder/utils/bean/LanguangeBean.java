06985f4d38364c71f8d1af0538f0b108c7cc4cfb
unknown <rm64640@L02MICRO28.fiap.com.br>
2013-09-05 18:48:53 -0300

    Criacao do Template e Arquivo de Internacionalizacao

---
+++ src/com/medfinder/utils/bean/LanguangeBean.java
@@ -0,0 +1,50 @@
+package com.medfinder.utils.bean;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LanguangeBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7548182994340105075L;
+	private Locale locale;
+	
+	@PostConstruct
+	private void init(){
+		locale = FacesContext.getCurrentInstance().getViewRoot()
+			.getLocale();
+	}
+
+	public void changeEnglish() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		UIViewRoot view = context.getViewRoot();
+		locale = Locale.ENGLISH;
+		view.setLocale(locale);
+	}
+
+	public void changePortuguese() {
+
+		FacesContext context = FacesContext.getCurrentInstance();
+		UIViewRoot view = context.getViewRoot();
+		locale = new Locale("pt", "BR");
+		view.setLocale(locale);
+	}
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+}
