9e6ef28a8276a843a126d1337a8fe5661f7028ec
unknown <rm64640@L02MICRO33.fiap.com.br>
2013-09-10 18:52:56 -0300

    MB Cadastro de usuario

---
+++ src/com/medfinder/MB/CadastroClienteBean.java
@@ -0,0 +1,35 @@
+package com.medfinder.MB;
+
+import java.util.Date;
+
+public class CadastroClienteBean {
+
+	private Date dataNasc;
+	private String senha;
+	private String cep;
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+}
