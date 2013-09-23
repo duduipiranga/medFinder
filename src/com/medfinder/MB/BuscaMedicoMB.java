76f70cd7e7e7f2d9b3158eda26b7e0863a34806f
unknown <rm65581@L06MICRO31.fiap.com.br>
2013-09-23 20:37:28 -0300

    fuck you

---
+++ src/com/medfinder/MB/BuscaMedicoMB.java
@@ -0,0 +1,138 @@
+package com.medfinder.MB;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+
+import com.medfinder.dao.impl.MedicoDAO;
+import com.medfinder.dao.impl.OperadoraDAO;
+import com.medfinder.dao.impl.PlanoDAO;
+import com.medfinder.entity.Medico;
+import com.medfinder.entity.Operadora;
+import com.medfinder.entity.Plano;
+
+@ManagedBean
+@ViewScoped
+public class BuscaMedicoMB implements Serializable{	
+	
+	private static final long serialVersionUID = 2814850488945255572L;
+
+
+	private List<Medico> medicos;	
+	
+	private List<Plano> planos;
+
+	private List<Operadora> operadoras;
+
+	private List<SelectItem> planosList;
+
+	private List<SelectItem> operadorasList;
+	
+	private Plano plano;
+	
+	private Operadora operadora;
+	
+	
+
+	public Plano getPlano() {
+		return plano;
+	}
+
+	public void setPlano(Plano plano) {
+		this.plano = plano;
+	}
+
+	public Operadora getOperadora() {
+		return operadora;
+	}
+
+	public void setOperadora(Operadora operadora) {
+		this.operadora = operadora;
+	}
+
+	PlanoDAO pdao = new PlanoDAO();
+	OperadoraDAO opdao = new OperadoraDAO();
+	MedicoDAO mdao = new MedicoDAO();
+
+	
+	
+	public List<Plano> getPlanos() {
+		return planos;
+	}
+
+	public void setPlanos(List<Plano> planos) {
+		this.planos = planos;
+	}
+
+	public List<Operadora> getOperadoras() {
+		return operadoras;
+	}
+
+	public void setOperadoras(List<Operadora> operadoras) {
+		this.operadoras = operadoras;
+	}
+
+	public List<SelectItem> getPlanosList() {
+		return planosList;
+	}
+
+	public void setPlanosList(List<SelectItem> planosList) {
+		this.planosList = planosList;
+	}
+
+	public List<SelectItem> getOperadorasList() {
+		return operadorasList;
+	}
+
+	public void setOperadorasList(List<SelectItem> operadorasList) {
+		this.operadorasList = operadorasList;
+	}	
+
+	public List<Medico> getMedicos() {
+		return medicos;
+	}
+
+	public void setMedicos(List<Medico> medicos) {
+		this.medicos = medicos;
+	}
+	
+	public String cadastrar() {
+		String retorno = "cadastroCliente?faces-redirect=true";
+
+		return retorno;
+	}
+
+	public String loginCliente() {
+		String retorno = "loginCliente?faces-redirect=true";
+
+		return retorno;
+
+	}
+	
+	
+	
+	@PostConstruct
+	public void init() {
+		operadorasList = new ArrayList<SelectItem>();
+		operadoras = opdao.listAll();
+
+	}
+	
+	public void listaPlanosAjax() {
+		System.out.println("Chegou nos planos");
+		planos = pdao.getPlanosByOperadora(operadora);
+		
+		
+		for (Plano p : planos) {
+			System.out.println(p.getDs_plano());
+		}
+		}
+	
+	
+
+}
