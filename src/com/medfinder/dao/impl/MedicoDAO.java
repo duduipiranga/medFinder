5955d59f12dc3d81645107f5c977804131846512
alephthiago <alephthiago@gmail.com>
2013-09-26 16:45:54 -0300

    medico

--- src/com/medfinder/dao/impl/MedicoDAO.java
+++ src/com/medfinder/dao/impl/MedicoDAO.java
@@ -1,16 +1,40 @@
 package com.medfinder.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.Query;
 
 import com.medfinder.entity.Especialidade;
 import com.medfinder.entity.Medico;
 import com.medfinder.entity.Plano;
 
-public class MedicoDAO extends HibernateDAO<Medico,String>{
-	
-	/*public List<Medico> retornaMedicosPorEspecialidade(Especialidade especialidade, Plano plano){
-		
-		
+public class MedicoDAO extends HibernateDAO<Medico, String> {
+
+	List<Medico> medicos = new ArrayList<Medico>();
+
+	public List<Medico> retornaMedicosSoPorEspecialidade(
+			Especialidade especialidade) {
+		Query q = em
+				.createQuery("From Medico m where m.especialidades = :id_especialidade");
+		q.setParameter("id_especialidade", especialidade.getId_especialidade());
+
+		medicos = q.getResultList();
+
+		return medicos;
 	}
-*/
+
+	public List<Medico> retornaMedicosPorEspecialidadePorPlano(
+			Especialidade especialidade, Plano plano) {
+		Query q = em
+				.createQuery("From Medico m where m.especialidades = :id_especialidade and m.planos = :id_plano");
+		q.setParameter("id_especialidade", especialidade.getId_especialidade());
+		q.setParameter("id_plano", plano.getId_plano());
+		
+		medicos = q.getResultList();
+		
+		
+		return medicos;
+	}
+
 }
---
+++ src/com/medfinder/teste/Teste3.java
@@ -0,0 +1,33 @@
+package com.medfinder.teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.medfinder.dao.impl.MedicoDAO;
+import com.medfinder.entity.Especialidade;
+import com.medfinder.entity.Medico;
+import com.medfinder.entity.Plano;
+
+public class Teste3 {
+	public static void main(String[] args) {
+		
+		MedicoDAO mdao = new MedicoDAO();
+		List<Medico> medicos = new ArrayList<Medico>();
+		
+		Plano p = new Plano();
+		p.setId_plano(13);
+		
+		Especialidade e = new Especialidade();
+		e.setId_especialidade(23);
+		
+		
+		medicos = mdao.retornaMedicosPorEspecialidadePorPlano(e, p);
+		
+		System.out.println("Teste comecando");
+		for (Medico medico : medicos) {
+			System.out.println(medico.getNome());
+		}
+		
+	}
+
+}
